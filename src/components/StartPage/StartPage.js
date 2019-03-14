7646ebe40d31b76f8eade67d394f39299d6d7566
MattMakers <wasik.matt@gmail.com>
2019-03-14 20:33:22 +0100

    initial commit

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,1 +1,32 @@
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 import './StartPage.sass'
+
+class StartPage extends Component {
+    startGame() {
+        return console.log('dupa')
+    }
+
+    render() {
+        return (
+            <div className="StartPage">
+                    <header className="StartPageHeader">
+                        <h1>AWESOME BATTLESHIPS</h1>
+                    </header>
+                    <div className="welcomeForm">
+                        <form className="myForm">
+                            <TextField
+                                id="name"
+                                label="Name"/>
+                            <Button variant="contained" color="secondary" size="medium" className="startButton">
+                                Jump IN
+                            </Button>
+                        </form>
+                    </div>
+            </div>
+        )
+    }
+}
+
+export default StartPage
