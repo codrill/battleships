55a3f46287cdcc81b2040cb83cfe29af79c089bf
Radomir Skrzepij <radek.s87@gmail.com>
2019-03-14 22:34:34 +0100

    trying to understand how states works

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,10 +1,39 @@
 import './StartPage.sass'
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class StartPage extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      name: '',
+      content: ''
+    };
+  }
+  
+  onGoNext = () => {
+    console.log('this.refs.name1.value', this.refs.name1.value);
+    this.setState({
+      name: this.refs.name1.value
+    });
+    
+    this.setState((state) => ({
+      content: !!state.name ? `Nice to have you here ${state.name}! ` : ''
+    }));
+    
+    console.log(this.state)
+  };
+  
   render() {
+    
     return (
-      <div>Start Page</div>
+      <div className="start-page">
+        <h1>Start Page</h1>
+        <input type="text" id="name1" ref="name1" placeholder="Provide your name"/>
+        <button onClick={this.onGoNext}>Go next!</button>
+        
+          <div>{ this.state.content }</div>
+      </div>
     )
   }
 }
