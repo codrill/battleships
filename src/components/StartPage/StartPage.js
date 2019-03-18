81f3aac468bad658a82bdfff58099683aad1022e
Mateusz Wasik <mwasik@future-processing.com>
2019-03-18 15:43:51 +0100

    refactor

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import './StartPage.sass'
 
 class StartPage extends Component {
-    startGame() {
-        return console.log('dupa')
-    }
+  startGame() {
 
-    render() {
-        return (
-            <div className="StartPage">
-                    <header className="StartPageHeader">
-                        <h1>AWESOME BATTLESHIPS</h1>
-                    </header>
-                    <div className="welcomeForm">
-                        <form className="myForm">
-                            <TextField
-                                id="name"
-                                label="Name"/>
-                            <Button variant="contained" color="secondary" size="medium" className="startButton">
-                                Jump IN
-                            </Button>
-                        </form>
-                    </div>
-            </div>
-        )
-    }
+  }
+
+  render() {
+    return (
+      <div className="StartPage">
+        <header className="StartPageHeader">
+          <h1>AWESOME BATTLESHIPS</h1>
+        </header>
+        <div className="welcomeForm">
+          <form className="myForm">
+            <TextField autoComplete="false"
+              id="name"
+              label="Name"/>
+            <Button variant="contained" color="secondary" size="medium" className="startButton">
+              Jump IN
+            </Button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default StartPage
