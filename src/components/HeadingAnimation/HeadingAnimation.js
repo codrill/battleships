976a3fe6d645ffd5bb077fe918da0ad776649026
Radomir Skrzepij <radek.s87@gmail.com>
2019-04-18 21:55:15 +0200

    add some global styles and to UnderConstruction component. Need to expand and use HeadingAnimation component to use it for each animated text.

---
+++ src/components/HeadingAnimation/HeadingAnimation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class HeadingAnimation extends React.Component {
+
+  render() {
+    return(
+      <h2>
+        {/*
+          TODO: Add functionality to use this component in UnderConstruction.
+          It should accept 'string' as props and use it to return animated text
+        */}
+      </h2>
+    )
+  }
+}
--- src/components/UnderConstruction/UnderConstruction.js
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
+import './UnderConstruction.sass'
 
 export default class UnderConstruction extends React.Component {
-
 	render() {
 		return (
-			<div className="underConstruction">
-				<h2>Site is under construction</h2>
+			<div className="under-construction">
+				<section className="under-construction__heading">
+					<h1>At this moment..</h1>
+					<h1>We are welding new BattleShips</h1>
+					<h1>Come back to us later</h1>
+				</section>
+				
 			</div>
 		)
 	}
