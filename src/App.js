12459c17bcb23dfbe12130948c5d10f4201f27c8
Mateusz <wasik.matt@gmail.com>
2019-04-20 17:26:58 +0200

    add path index->app->approuter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, {Component} from 'react'
 import './App.sass'
-import UnderConstruction from './components/UnderConstruction/UnderConstruction'
-import {Route, Switch} from "react-router";
-import StartPage from "./components/StartPage/StartPage";
-import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./AppRouter";
 
 export default class App extends Component {
-  render() {
-    return (
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={UnderConstruction }/>
-            <Route path="/dev" component={StartPage }/>
-          </Switch>
-        </BrowserRouter>
-    )
-  }
+    render() {
+        return (
+            <AppRouter/>
+        )
+    }
 }
