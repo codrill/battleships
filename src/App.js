c92c1d349f1e56a9b64edd2cd162aea081fd6ad7
Mateusz <wasik.matt@gmail.com>
2019-04-20 15:25:13 +0200

    add routing and /dev route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react'
 import './App.sass'
 import UnderConstruction from './components/UnderConstruction/UnderConstruction'
+import {Route, Switch} from "react-router";
+import StartPage from "./components/StartPage/StartPage";
+import {BrowserRouter} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div><UnderConstruction/></div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={UnderConstruction }/>
+            <Route path="/dev" component={StartPage }/>
+          </Switch>
+        </BrowserRouter>
     )
   }
 }
--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,15 +3,16 @@ import {Route} from 'react-router'
 import React, {Component} from 'react'
 import UnderConstruction from './components/UnderConstruction/UnderConstruction'
 import StartPage from "./components/StartPage/StartPage";
+import Switch from "react-router/Switch";
 
 class AppRouter extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Route path="/" component={UnderConstruction} exact/>
+        <Switch>
+          <Route exact path="/" component={UnderConstruction}/>
           <Route path="/dev" component={StartPage}/>
-        </div>
+        </Switch>
       </BrowserRouter>
     )
   }
