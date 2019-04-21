21b22bed2ab475d19aa57624c91cbe94c1c31cba
Radomir Skrzepij <radek.s87@gmail.com>
2019-04-22 01:01:08 +0200

    create new component which will be rosponsible for text animations; add some styles, basic logotype and required functionality to under construction page

--- src/components/HeadingAnimation/HeadingAnimation.js
+++ src/components/HeadingAnimation/HeadingAnimation.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import * as React from 'react'
+import './HeadingAnimation.sass'
 
 export default class HeadingAnimation extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      animatedTexts: props.animatedTexts,
+      currentTextToAnimate: props.animatedTexts[0],
+      textSize: props.textSize
+    }
+  }
+  
+  componentDidMount() {
+    this.currentTextIndex = 0
+    this.changeText = setInterval(this.updateText.bind(this), 5000)
+  }
+  
+  updateText() {
+    // console.log('currentTextToAnimate index', this.state.animatedTexts.indexOf(this.state.currentTextToAnimate))
+    
+    this.currentTextIndex++
+  
+    this.setState({
+      currentTextToAnimate: this.state.animatedTexts[this.currentTextIndex]
+    })
+    
+    console.log('state', this.currentTextIndex, this.state.currentTextToAnimate)
+    if (this.currentTextIndex === this.state.animatedTexts.length - 1)
+      clearInterval(this.changeText)
+  }
+  
 
   render() {
     return(
-      <h2>
-        {/*
-          TODO: Add functionality to use this component in UnderConstruction.
-          It should accept 'string' as props and use it to return animated text
-        */}
-      </h2>
+      <div className="heading-animation">
+        <h2 style={{fontSize: this.props.textSize, animationIterationCount: this.props.animatedTexts.length}}>
+          { this.state.currentTextToAnimate }
+        </h2>
+      </div>
     )
   }
 }
--- src/components/UnderConstruction/UnderConstruction.js
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -1,14 +1,53 @@
 import * as React from 'react'
 import './UnderConstruction.sass'
+import HeadingAnimation from '../HeadingAnimation/HeadingAnimation'
+import logo from '../../assets/images/logo.png'
+
+const textsArray = [
+	'At this moment..',
+	'We are welding new BattleShips',
+	'Come back to us later'
+]
 
 export default class UnderConstruction extends React.Component {
+	// constructor() {
+	// 	super()
+	// 	// this.state = {headingText: textsArray[0]}
+	// 	// setTimeout(() => {
+	// 	// 	this.setState({headingText: textsArray[1]})
+	// 	// }, 500)
+	// }
+	
+	componentDidMount() {
+			// when the state is updated (turned red),
+			// a timeout is triggered to switch it back off
+			// setTimeout(() => {
+			// 	this.setState({headingText: textsArray[1]})
+			// }, 3000);
+	}
+	
+	generateAnimatedText(...texts) {
+		// const createdText = []
+		// texts.forEach((text, index) => {
+		// 	createdText.push(<HeadingAnimation key={index} animatedText={text} textSize={`2.5rem`} />)
+		// })
+		// setTimeout(() => {
+		// 	this.setState({headingText: textsArray[1]})
+		// }, 300)
+		
+	}
+	
 	render() {
 		return (
 			<div className="under-construction">
+				<header>
+					<img src={logo} alt=""/>
+				</header>
 				<section className="under-construction__heading">
-					<h1>At this moment..</h1>
-					<h1>We are welding new BattleShips</h1>
-					<h1>Come back to us later</h1>
+					
+					{
+						<HeadingAnimation animatedTexts={textsArray} textSize={`4rem`} />
+					}
 				</section>
 				
 			</div>
