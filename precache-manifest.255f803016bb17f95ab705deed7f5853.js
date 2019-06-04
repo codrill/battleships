5d49fd69fe764539a4e8ffe126ae20b582573467
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-04 18:04:13 +0000

    Deploy mad-rat/battleships to github.com/mad-rat/battleships.git:master

---
+++ precache-manifest.255f803016bb17f95ab705deed7f5853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95135236b487deda1abb",
+    "url": "/battleships/static/js/runtime~main.09af5945.js"
+  },
+  {
+    "revision": "8fa212ab547a3eda3ba2",
+    "url": "/battleships/static/js/main.1d331506.chunk.js"
+  },
+  {
+    "revision": "dcc421ab4c4fc93b2b27",
+    "url": "/battleships/static/js/2.0974e202.chunk.js"
+  },
+  {
+    "revision": "8fa212ab547a3eda3ba2",
+    "url": "/battleships/static/css/main.5cbc6243.chunk.css"
+  },
+  {
+    "revision": "98be92a2551ffcca662f7919ce679c67",
+    "url": "/battleships/index.html"
+  }
+];
